a2ed3798693a95fa86fbc7e5a2b0968d872ba0df
Dr.chen <1191180437@qq.com>
2019-10-05 18:47:33 +0800

    My Third Edit Commit

--- src/com/yangc/test/Demo.java
+++ src/com/yangc/test/Demo.java
@@ -9,5 +9,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("第一次编辑操作");
         System.out.println("第二次编辑操作");
+        System.out.println("第三次编辑操作");
     }
 }
