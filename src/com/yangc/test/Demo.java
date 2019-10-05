f44ff212b5564e2b47e84ae255fc13a50e670363
Dr.chen <1191180437@qq.com>
2019-10-05 18:44:43 +0800

    My First Edit Commit

---
+++ src/com/yangc/test/Demo.java
@@ -0,0 +1,12 @@
+package com.yangc.test;
+
+/**
+ * @author：Dr.chen
+ * @date：2019/10/5 18:42
+ * @Description：
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("第一次编辑操作");
+    }
+}
---
+++ src/com/yangc/test/HelloGit.java
@@ -0,0 +1,12 @@
+package com.yangc.test;
+
+/**
+ * @author：Dr.chen
+ * @date：2019/10/5 18:06
+ * @Description：
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+
+    }
+}
