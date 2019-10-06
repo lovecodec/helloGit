62e0c44ad42116227cf663a89b20c076d129535d
Dr.chen <1191180437@qq.com>
2019-10-06 16:01:27 +0800

    Hello Controller

---
+++ src/com/yangc/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.yangc.controller;
+
+/**
+ * @author：Dr.chen
+ * @date：2019/10/6 15:59
+ * @Description：
+ */
+public class HelloController {
+
+    public String sayHello(){
+        return "hello";
+    }
+
+}
